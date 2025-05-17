0260b01f8a64e4ccd1f9f3f6a0a4783cb333491c
Ghimirebijay607 <bijayghimire184@gmail.com>
2025-05-17 18:00:11 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function toggleInfo() {
+    let info = document.getElementById("extra-info");
+    if (info.style.display === "none") {
+        info.style.display = "block";
+    } else {
+        info.style.display = "none";
+    }
+}
 
